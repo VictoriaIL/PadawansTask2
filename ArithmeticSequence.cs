609a32aefe4ce88300be7917da9127cacd146faf
VictoriaIL <47503735+VictoriaIL@users.noreply.github.com>
2019-02-18 00:40:15 +0300

    try

--- ArithmeticSequence.cs
+++ ArithmeticSequence.cs
@@ -6,8 +6,26 @@ namespace PadawansTask2
     {
         public static int Calculate(int number, int add, int count)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (count < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            try
+            {
+                int i = 0;
+                int result = 0;
+                while (i < count)
+                {
+                    result += number + i * add;
+                    i++;
+                }
+            }
+            catch( OverflowException)
+            {
+                throw new OverflowException();
+            }
+            return result;
         }
     }
 }
